2c4bfccbbab26efa0c234fcaab215323ce8e8548
yujin <yujin@naver.com>
2018-03-05 16:09:34 +0900

    Ex13

---
+++ src/Exercise03/Ex13.java
@@ -0,0 +1,15 @@
+package Exercise03;
+
+public class Ex13 {
+
+	public static void main(String[] args) {
+		for(int i=2;i<=9;i++) {
+			System.out.println("## "+i+"단 ##");
+			for(int j=1;j<=9;j++) {
+				System.out.println(i+" * "+j+" = "+i*j);
+			}
+		}
+
+	}
+
+}
