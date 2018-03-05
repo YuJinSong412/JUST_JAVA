de702d674c523c6b35b916cb6745dbb5ef0ad0ca
yujin <yujin@naver.com>
2018-03-05 17:31:45 +0900

    Prac02

---
+++ src/Practice03/Prac02.java
@@ -0,0 +1,13 @@
+package Practice03;
+
+public class Prac02 {
+
+	public static void main(String[] args) {
+		char c = 'k';
+		
+		System.out.printf("%c\n", c);
+		System.out.printf("%d\n", (int)c);
+
+	}
+
+}
