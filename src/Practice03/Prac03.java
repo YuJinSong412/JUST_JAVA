977ee855f401d136786042395a9eff59329c2da6
yujin <yujin@naver.com>
2018-03-05 19:08:19 +0900

    Prac03

--- src/Practice03/Prac03.java
+++ src/Practice03/Prac03.java
@@ -8,20 +8,18 @@ public class Prac03 {
 		
 		int H_money = 10000;
 		
-		while(true) {
+		while(H_money>0) {
 		System.out.print("사용할 금액을 입력하세요: ");
 		int U_money = scanner.nextInt();
 		
 		H_money -= U_money;
 		System.out.println(H_money);
-		if(H_money>0)
-			continue;
+		
+	
+		}
 		if(H_money<=0) { 
 			System.out.println("잔액이 부족해 교통 카드를 사용할 수 없습니다!!");
 		}
-		break;
-		}
-
 	}
 
 }
