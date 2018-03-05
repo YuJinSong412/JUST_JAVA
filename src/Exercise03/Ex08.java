b4b006d677fe15d2738cfa57f756ed27d614d906
yujin <yujin@naver.com>
2018-03-05 15:16:48 +0900

    ex08

---
+++ src/Exercise03/Ex08.java
@@ -0,0 +1,23 @@
+package Exercise03;
+import java.util.Scanner;
+
+public class Ex08 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("##메뉴 선택##");
+		System.out.println("[1]구매자 [2]판매자");
+		System.out.print("##메뉴를 선택 하세요->");
+		
+		int num = scanner.nextInt();
+		if(num==1) {
+			System.out.println("환영합니다! 구매자로 로그인하셨습니다.");
+		}
+		if(num==2) {
+			System.out.println("환영합니다! 판매자로 로그인하셨습니다.");
+		}
+
+	}
+
+}
