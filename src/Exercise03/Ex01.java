bc70fc61509a2692a2bf5282de334a27c23479bf
yujin <yujin@naver.com>
2018-03-05 13:42:21 +0900

    src

---
+++ src/Exercise03/Ex01.java
@@ -0,0 +1,14 @@
+package Exercise03;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		int x = 12;
+		int y =20;
+		int result = x+y;
+		System.out.println(x+"+"+y+"="+result);
+		System.out.printf("%d+%d=%d",x,y,result);
+
+	}
+
+}
